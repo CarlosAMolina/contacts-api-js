12d27cacfb45c07cacd953499d9ec148cb60e7d1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-03 02:16:07 +0200

    refactor(query): sort

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,13 +2,13 @@ import { getDB } from '../db/db.js'
 
 export const resolvers = {
     Query: {
-        totalContacts: async () => {
-            const { contacts } = await getDB()
-            return contacts.length
-        },
         allContacts: async () => {
             const { contacts } = await getDB()
             return contacts
+        },
+        totalContacts: async () => {
+            const { contacts } = await getDB()
+            return contacts.length
         }
     }
 }
