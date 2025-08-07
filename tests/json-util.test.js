b83c9b77043e8b1fcf2e07b67fefc7d808aa8ce4
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 01:58:48 +0200

    test(json-util): add test

--- tests/json-util.test.js
+++ tests/json-util.test.js
@@ -1,7 +1,13 @@
 import { getContactsMatched } from '../src/json-util.js'
 
-describe('getContactsMatched', () => {
-    test('value in name is matched', () => {
+describe('getContactsMatched matches if the value is in different keys', () => {
+    test('key id', () => {
+        const contacts = [
+            {"id": 1}
+        ]
+        expect([{"id": 1}]).toEqual(getContactsMatched(contacts, "1"));
+    });
+    test('key name', () => {
         const contacts = [
             {"name": "foo"}
         ]
