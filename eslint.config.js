c6571b84b7ac04cb1caca9a1e661db10a349ce3c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 01:44:17 +0200

    chore,style(eslint): check indentation and test files

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,26 @@
 import js from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-      files: ["**/*.{js,mjs,cjs}"],
-      plugins: {
-          js
-      },
-      extends: ["js/recommended"],
-      languageOptions: {
-          globals: globals.browser
-      },
-      rules: {
-          "no-unused-vars": "warn",
-          "no-undef": "warn",
-      },
-  },
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: {
+            js,
+            jest: pluginJest
+        },
+        extends: ["js/recommended"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.jest
+            }
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
+            "indent": ["error", 4]
+        },
+    },
 ]);
-
