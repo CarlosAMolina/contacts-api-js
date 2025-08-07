951b7f83bbc8660ebb3910d0f702ca007644a465
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 01:24:10 +0200

    test(json-util): add test

---
+++ tests/json-util.test.js
@@ -0,0 +1,10 @@
+import { getContactsMatched } from '../src/json-util.js'
+
+describe('getContactsMatched', () => {
+  test('value in name is matched', () => {
+    const contacts = [
+        {"name": "foo"}
+    ]
+      expect([{"name": "foo"}]).toEqual(getContactsMatched(contacts, "foo"));
+  });
+})
