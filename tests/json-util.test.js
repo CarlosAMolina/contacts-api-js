f636ee41f20c0e282caff3f34b2f8121f48483bc
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 02:09:28 +0200

    test(json-util): add test and change description

--- tests/json-util.test.js
+++ tests/json-util.test.js
@@ -1,13 +1,21 @@
 import { getContactsMatched } from '../src/json-util.js'
 
-describe('getContactsMatched matches if the value is in different keys', () => {
-    test('key id', () => {
+describe('getContactsMatched matches differente values', () => {
+    test('integer', () => {
         const contacts = [{"id": 1}]
         expect([{"id": 1}]).toEqual(getContactsMatched(contacts, "1"));
     });
     test('key addresses', () => {
         const contacts = [{"addresses": ["foo"]}]
         expect([{"addresses": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
+    });
+    test('key categories', () => {
+        const contacts = [{"categories": ["foo"]}]
+        expect([{"categories": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
+    });
+    test('key emails', () => {
+        const contacts = [{"emails": ["foo"]}]
+        expect([{"emails": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
     });
     test('key name', () => {
         const contacts = [{"name": "foo"}]
