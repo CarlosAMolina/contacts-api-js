43c9871e15113dc1683610c5b665328fc73f0416
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 02:07:52 +0200

    test(json-util): add test

--- tests/json-util.test.js
+++ tests/json-util.test.js
@@ -5,6 +5,10 @@ describe('getContactsMatched matches if the value is in different keys', () => {
         const contacts = [{"id": 1}]
         expect([{"id": 1}]).toEqual(getContactsMatched(contacts, "1"));
     });
+    test('key addresses', () => {
+        const contacts = [{"addresses": ["foo"]}]
+        expect([{"addresses": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
+    });
     test('key name', () => {
         const contacts = [{"name": "foo"}]
         expect([{"name": "foo"}]).toEqual(getContactsMatched(contacts, "foo"));
