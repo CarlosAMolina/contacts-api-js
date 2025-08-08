ff6dadbf020ce26ff73115b40bf94af952497721
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 02:19:00 +0200

    test(json-util): style apply eslint

--- tests/json-util.test.js
+++ tests/json-util.test.js
@@ -2,25 +2,25 @@ import { getContactsMatched } from '../src/json-util.js'
 
 describe('getContactsMatched', () => {
     describe('matches differente values', () => {
-    test('array of objects', () => {
-        const contacts = [{"phones": [{"description": "foo"}]}]
-        expect([{"phones": [{"description": "foo"}]}]).toEqual(getContactsMatched(contacts, "foo"));
-    });
-    test('array of strings', () => {
-        const contacts = [{"addresses": ["foo"]}]
-        expect([{"addresses": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
-    });
-    test('integer', () => {
-        const contacts = [{"id": 1}]
-        expect([{"id": 1}]).toEqual(getContactsMatched(contacts, "1"));
-    });
-    test('object of objects', () => {
-        const contacts = [{"socialNetwork":{"discordAccounts":[{"alias": "foo"}]}}]
-        expect([{"socialNetwork":{"discordAccounts":[{"alias": "foo"}]}}]).toEqual(getContactsMatched(contacts, "foo"));
-    });
-    test('string', () => {
-        const contacts = [{"name": "foo"}]
-        expect([{"name": "foo"}]).toEqual(getContactsMatched(contacts, "foo"));
-    });
+        test('array of objects', () => {
+            const contacts = [{"phones": [{"description": "foo"}]}]
+            expect([{"phones": [{"description": "foo"}]}]).toEqual(getContactsMatched(contacts, "foo"));
+        });
+        test('array of strings', () => {
+            const contacts = [{"addresses": ["foo"]}]
+            expect([{"addresses": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
+        });
+        test('integer', () => {
+            const contacts = [{"id": 1}]
+            expect([{"id": 1}]).toEqual(getContactsMatched(contacts, "1"));
+        });
+        test('object of objects', () => {
+            const contacts = [{"socialNetwork":{"discordAccounts":[{"alias": "foo"}]}}]
+            expect([{"socialNetwork":{"discordAccounts":[{"alias": "foo"}]}}]).toEqual(getContactsMatched(contacts, "foo"));
+        });
+        test('string', () => {
+            const contacts = [{"name": "foo"}]
+            expect([{"name": "foo"}]).toEqual(getContactsMatched(contacts, "foo"));
+        });
     });
 })
