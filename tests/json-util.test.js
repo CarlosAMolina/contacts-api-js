ef0746841e8aea44eb5dbbbd17b5d10472a7c7fe
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-08 02:10:11 +0200

    test(json-util): drop similar tests and change description

--- tests/json-util.test.js
+++ tests/json-util.test.js
@@ -5,19 +5,11 @@ describe('getContactsMatched matches differente values', () => {
         const contacts = [{"id": 1}]
         expect([{"id": 1}]).toEqual(getContactsMatched(contacts, "1"));
     });
-    test('key addresses', () => {
+    test('array of strings', () => {
         const contacts = [{"addresses": ["foo"]}]
         expect([{"addresses": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
     });
-    test('key categories', () => {
-        const contacts = [{"categories": ["foo"]}]
-        expect([{"categories": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
-    });
-    test('key emails', () => {
-        const contacts = [{"emails": ["foo"]}]
-        expect([{"emails": ["foo"]}]).toEqual(getContactsMatched(contacts, "foo"));
-    });
-    test('key name', () => {
+    test('string', () => {
         const contacts = [{"name": "foo"}]
         expect([{"name": "foo"}]).toEqual(getContactsMatched(contacts, "foo"));
     });
